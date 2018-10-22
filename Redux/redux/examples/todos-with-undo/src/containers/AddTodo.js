cbf9fb6bc81b3a8affebd46259fe4e4e00da84f2
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2018-10-22 03:23:11 -0700

    Update AddTodo.js. From stateless function component=>stateful component

--- Redux/redux/examples/todos-with-undo/src/containers/AddTodo.js
+++ Redux/redux/examples/todos-with-undo/src/containers/AddTodo.js
@@ -1,47 +1,83 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addTodo, saveTodo } from '../actions'
+import * as TodoActions  from '../actions'
 
-let AddTodo = ({ dispatch }) => {
-  let input, inputid;
-  let saving_id = 0;
-  return (
-    
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        saving_id = Number.parseInt(inputid.value, 10);//!inputid.value.trim();
-        if (!saving_id)
-        {
-          dispatch(addTodo(input.value))
-        }
-        else 
-        {
-         
-          dispatch(saveTodo(saving_id, input.value))
-        }
-        if (!saving_id)
-          input.value = ''
-      }}>
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { id: -1, text:'' };
+    this.handle_addTodo=this.handle_addTodo.bind(this);
+    this.handle_saveTodo=this.handle_saveTodo.bind(this);
+  }
+
+  handle_saveTodo(text)
+  {
+    this.setState({...this.state, text:text});
+    this.props.saveTodo(this.state.id, text);
+  }
+
+  handle_addTodo(text)
+  {
+    this.props.addTodo(text)
+  }
+
+  render()
+  {
+    let input;
+    let issaving = 0;
+
+    return (
+      <div>
+        <form onSubmit={e => {
+          e.preventDefault()
+          if (!input.value.trim()) {
+            return
+          }
+          issaving = this.state.id >= 0;//Number.parseInt(inputid.value, 10);//!inputid.value.trim();
+          
+          if (issaving)
+          {
+            input.value = this.state.text;
+            this.handle_saveTodo(input.value);
+          }
+          else
+          {
+            this.handle_addTodo(input.value)
+          }
+
+            
+        }}>
         <input ref={node => {
           input = node
         }} />
-        
-        <input ref={node => {
-          inputid = node
-        }} />
+        { !issaving? 
+          <button type="submit">
+            Add Todo
+          </button>
+          :
+          <button type="submit">
+            Save Todo}
+          </button>
+        }
+          
 
-        <button type="submit">
-          {!saving_id ? "Add Todo" : "Save Todo"}
-        </button>
-
-      </form>
-    </div>
-  )
+        </form>
+      </div>
+    )
+  }
 }
-AddTodo = connect()(AddTodo)
+const mapStatetoProps = (state) =>
+({
+  id: state.id,
+  text: state.text
+})
+const mapDispatchtoProps = (dispatch) =>
+({
+  addTodo:(text) => dispatch(TodoActions.addTodo(text)),
+  saveTodo:(id, text) => dispatch(TodoActions.saveTodo(id, text))
+})
+AddTodo = connect(mapStatetoProps, mapDispatchtoProps)(AddTodo)
+
 
 export default AddTodo
+
