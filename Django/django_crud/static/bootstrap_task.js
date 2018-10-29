35f8aaceaabe3e3a3e6e6c4505926a78d16bfe66
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2018-10-29 12:31:08 -0700

    Add check_validate

--- Django/django_crud/static/bootstrap_task.js
+++ Django/django_crud/static/bootstrap_task.js
@@ -15,9 +15,29 @@ $( document ).ready( function() {
     $("#cmdCreate").prop("disabled", true);
    
   });
+  check_validate = function()
+  {
+    if (!$("#name").val().length)
+    {
+      alert("Please Input 'Name' field!");
+      return false;
+    }
+    if (!$("#email").val().length)
+    {
+      alert("Please Input 'Email' field!");
+      return false;
+    }
 
+    if (!$("#note").val().length)
+    {
+      alert("Please Input 'Note' field!");
+      return false;
+    }
+    return true;
+  }
   show_page = function(actionurl)
   {
+    
     request = $.ajax({
       url: actionurl,
       type: "post",
@@ -40,15 +60,21 @@ $( document ).ready( function() {
   }
 
   $("#cmdCreate").click(function() {
+    
+    if (!check_validate())
+       return;  
     show_page("new");
   });
  
   $( ".seldelete" ).click(function() {
+    
     dataid=$(this).data("id");
     show_page("delete/" + dataid);
   });
 
   $("#cmdUpdate").click(function() {
+    if (!check_validate())
+      return;
     show_page("edit/" + dataid);
   });
 
